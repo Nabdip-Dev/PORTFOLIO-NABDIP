c7c384c3231fd5fc555ff91a28de9d6e712f09db
NabdipDEV <nabdipdevsharma@gmail.com>
2026-09-19 13:00:42 +0530

    fix: navbar import

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ar, Navbar } from "@/components/layout/Navbar";
+import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/hero/Hero";
 import { About } from "@/components/about/About";
